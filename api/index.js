23edee76ab2836cca575ef196746d2e2146edfff
tobywerthan <tobywerthan1@gmail.com>
2022-06-21 11:42:34 -0500

    installation, nodejs and mongodb connection, create api using nodejs, create a hotel model

---
+++ api/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+//middlewares
+
+app.use("/api/auth", authRoute);
+app.use("/api/hotels", authRoute);
+app.use("/api/rooms", authRoute);
+app.use("/api/users", authRoute);
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend...");
+});
